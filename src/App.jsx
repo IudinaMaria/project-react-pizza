c7b178fbe3697cdcb03f5835a9b6ac4875e876e5
Maria Iudina <gretarichter@gmail.com>
2025-05-09 13:08:49 +0300

    edited README and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { Routes, Route } from 'react-router';
-import { useSelector, useDispatch } from 'react-redux'
+import { Routes, Route } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
-import { setCategoryId, setSort } from './redux/slices/filterSlice'
-
+import { setCategoryId, setSort } from "./redux/slices/filterSlice";
 
 import "./scss/app.scss";
 
@@ -17,21 +16,19 @@ function App() {
   const [searchValue, setSearchValue] = React.useState(""); // создаем состояние для поиска грубо говоря глобальную, которую передаем далее в хедер, потом в поиск и потом из поиска достаем эти значения
 
   return (
-    <div className ="wrapper">
-
+    <div className="wrapper">
       <SearchContext.Provider value={{ searchValue, setSearchValue }}>
-      <Header />
-      <div className ="content">
-        <div className ="container">
-          <Routes>
-<Route path="/" element={<Home />} />
-<Route path="/cart" element={<Cart />} />
-<Route path="*" element={<NotFound />} />
-
-          </Routes>  
+        <Header />
+        <div className="content">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
           </div>
-      </div > 
-        </SearchContext.Provider>
+        </div>
+      </SearchContext.Provider>
     </div>
   );
 }
